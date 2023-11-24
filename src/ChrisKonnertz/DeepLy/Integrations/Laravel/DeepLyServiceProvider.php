e653862d37d0e2a1e4a133d8a7295489fdbaa005
grafst <grafst@users.noreply.github.com>
2023-11-24 04:04:54 +0100

    add install command

--- src/ChrisKonnertz/DeepLy/Integrations/Laravel/DeepLyServiceProvider.php
+++ src/ChrisKonnertz/DeepLy/Integrations/Laravel/DeepLyServiceProvider.php
@@ -1,13 +1,13 @@
 <?php
 
 namespace ChrisKonnertz\DeepLy\Integrations\Laravel;
-
 use ChrisKonnertz\DeepLy\DeepLy;
 use Illuminate\Support\ServiceProvider;
+use Spatie\LaravelPackageTools\Commands\InstallCommand;
+use Spatie\LaravelPackageTools\Package;
 
 class DeepLyServiceProvider extends ServiceProvider
 {
-
     public function register()
     {
         $this->app->bind('deeply', function()
@@ -17,4 +17,23 @@ class DeepLyServiceProvider extends ServiceProvider
         });
     }
 
+    public function configurePackage(Package $package): void
+    {
+        /*
+         * This class is a Package Service Provider
+         *
+         * More info: https://github.com/spatie/laravel-package-tools
+         */
+        $package
+            ->name('DeepLy')
+            ->hasConfigFile()
+            //->hasMigration('')
+            ->hasInstallCommand(function (InstallCommand $command) {
+                $command
+                    ->publishConfigFile()
+                    ->publishMigrations()
+                    ->askToRunMigrations()
+                    ->copyAndRegisterServiceProviderInApp();
+            });
+    }
 }
